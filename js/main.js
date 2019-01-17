3899588265c46d743273c385519efdd5bc0f492f
Patel <a_patel73563@tss-hr420-dm27.fc.ca>
2019-01-17 16:39:07 -0500

    added child components

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@
     const UserComponent = {
         props: ['name', 'role'],
 
-        template: "#userstemplate"
+        template: "#userstemplate",
+
+        created: function(){
+            console.log("I'm aliiiiiive!");
+        },
+        methods: {
+            logFromChild(){
+                console.log("logged from the component");
+            },
+            passEvent(){
+                this.$emit('shoutup');
+            }
+        }
     }
 
     // then your vue instance
@@ -14,7 +26,12 @@
             testmessage: "sup"
         },
         methods:{
-
+            calledOnParent(){
+                console.log("This method lives in the main VM and wass cxalled a component");
+            }
+        },
+        created: function(){
+            console.log("Main Vue instance");
         },
         components: {
             'activeusers': UserComponent
