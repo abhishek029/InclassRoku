78cbd82edea737022b3a877063fd8fae4969eedd
Patel <a_patel73563@tss-hr420-dm27.fc.ca>
2019-01-31 17:09:26 -0500

    login working

--- js/main.js
+++ js/main.js
@@ -1,100 +1,43 @@
-(()=>{
-    // create a component first
-    const HomePageComponent = {
-        template: "<h2>This is the home page</h2>"
-    }
+import LoginComponent from '../js/modules/LoginComponent.js';
 
-    const UsersPageComponent = {
-        props: ['id'],
+const routes = [
+    { path: '/', redirect:{name: 'login'}},
+    { path: '/login', name: 'login', component: LoginComponent},
+]
+const router = new VueRouter({
+    routes
+});
+// then your vue instance
 
-        template: "#userList",
+const vm = new Vue({
+    data:{
+        testmessage: "sup",
 
-        data: function(){
-            return {
-                users: []
-            }
-        },
+        authenticated: false,
 
-        created: function(){
-            console.log('user component created');
-            this.fetchUserData(this.id);
-        },
-        methods:{
-            fetchUserData(users){
-                let url=`./includes/index.php?users=${this.id}`;
-
-                fetch(url)
-                    .then(res => res.json())
-                    .then(data => this.users = data)
-                .catch(function(error){
-                    console.error(error);
-                })
-            }
+        mockAccount:{
+            username: "abc",
+            password: "123"
         }
-    }
-    const ContactPageComponent = {
-        props: ['id'],
-
-        template: "#userList",
-
-        data: function(){
-            return {
-                users: []
-            }
-        },
-
-        created: function(){
-            console.log('user component created');
-            this.fetchUserData(this.id);
-        },
-        methods:{
-            fetchUserData(users){
-                let url=`./includes/index.php?users=${this.id}`;
-
-                fetch(url)
-                    .then(res => res.json())
-                    .then(data => this.users = data)
-                .catch(function(error){
-                    console.error(error);
-                })
-            }
+    },
+    methods:{
+        calledOnParent(){
+            console.log("This method lives in the main VM and wass cxalled a component");
         }
+    },
+    created: function(){
+        console.log("Main Vue instance");
+    },
+
+    router:router
+}).$mount("#app");
+
+router.beforeEach((to, from, next)=>{
+    console.log('router guard fired');
+
+    if(vm.authenticated == false){
+        next("/login");
+    }else{
+        next();
     }
-    const ErrorPageComponent = {
-        template: "<h2>Page not found! Try again...</h2>"
-    }
-
-    const routes = [
-        { path: '/', name: 'home', component: HomePageComponent},
-        { path: '/contact/:id', name: 'contact', component: ContactPageComponent, props: true},
-        { path: '/users/:id', name: 'users', component: UsersPageComponent, props: true},
-        { path: '/*', name: 'error', component: ErrorPageComponent }
-    ]
-    const router = new VueRouter({
-        routes
-    });
-    // then your vue instance
-
-    const vm = new Vue({
-        el:"#app",
-        data:{
-            testmessage: "sup"
-        },
-        methods:{
-            calledOnParent(){
-                console.log("This method lives in the main VM and wass cxalled a component");
-            }
-        },
-        created: function(){
-            console.log("Main Vue instance");
-        },
-        components: {
-            'homepagecomponent': HomePageComponent,
-            'userspagecomponent': UsersPageComponent,
-            'contactcomponent': ContactPageComponent
-
-        },
-
-        router:router
-    });
-})();
+});
