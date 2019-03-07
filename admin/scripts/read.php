b18785e208893eb9f4566b5d2a98cad3fa1a472d
Patel <a_patel73563@tss-hr420-dm27.fc.ca>
2019-03-07 17:06:24 -0500

    all working in class

--- admin/scripts/read.php
+++ admin/scripts/read.php
@@ -53,6 +53,10 @@ function filterResults($tbl,$tbl_2,$tbl_3,$col,$col_2,$col_3,$filter){
 
 	$runQuery = $pdo->query($filterQuery);
 	if($runQuery){
+		$results = array();
+		while($row = $runQuery->fetch(PDO::FETCH_ASSOC)){
+			$results[] = $row;
+		}
 		return $runQuery;
 	}else{
 		$error = 'There was a problem';
